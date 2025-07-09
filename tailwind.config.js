bad76939c497fefa7effa405b909cd1b6ba4bc5e
joseagl90 <gl.josea90@gmail.com>
2025-07-09 20:00:44 +0200

    De 0

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
