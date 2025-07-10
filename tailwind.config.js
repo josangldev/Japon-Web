49308beee1004f4c073b73bbb52792483cff5b4c
joseagl90 <gl.josea90@gmail.com>
2025-07-10 10:42:56 +0200

    actualizando tailwind a la version 3.4.17

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html}"
+    "./*.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        tori: '#C1272D',
+        sakura: '#F9CCD3',
+        dorado: '#FFD700',
+        'dark-header': '#A21CAF',
+      },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
+  darkMode: 'class',
+} 
